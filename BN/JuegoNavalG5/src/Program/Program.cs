d84d8abbfdbd527785f3b755ed89c3a4bdc3a71f
JoacoG11 <101676686+JoacoG11@users.noreply.github.com>
2022-07-07 21:05:38 -0300

    Add files via upload

---
+++ BN/JuegoNavalG5/src/Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PII_Batalla_Naval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Player p1 = new Player("joaco");
+            Player p2 = new Player("tareaP2");
+            Game g1 = new Game(p1, p2);
+            Logic log1 =  new Logic(g1);
+            log1.LogGame();
+            Console.Clear();
+            log1.LetsPlay();
+            Console.WriteLine(p1.PlayerBoard);*/
+
+            BotInConsole.Instance.StartCommunication();
+        }
+    }
+}
